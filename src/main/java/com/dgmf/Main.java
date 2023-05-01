37803ec23e83c19c6bb78f3855702cdc511feed9
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-01 06:17:07 +0200

    Retrieving a Bean 'cart' (of Type ShoppingCart) from the instance of the IoC container 'context'. Call of the checkout() method of the Bean cart ==> Ok

--- src/main/java/com/dgmf/Main.java
+++ src/main/java/com/dgmf/Main.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
+        // Instanciation du conteneur IoC basée sur la classe
+        // de configuration BeanConfig
         ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+
+        // Récupération d'un Bean de Type ShoppingCart
+        ShoppingCart cart = context.getBean(ShoppingCart.class);
+        cart.checkout();
     }
 }
