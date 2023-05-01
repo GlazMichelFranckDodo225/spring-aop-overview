1a3bf28cc5ec0cdfb488e91d20c08938d75f1cf2
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-01 05:49:57 +0200

    Creation of the Main driver class and the ShoppingCart class (with its checkout() method), the latter having the @Component annotation

---
+++ src/main/java/com/dgmf/ShoppingCart.java
@@ -0,0 +1,10 @@
+package com.dgmf;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCart {
+    public void checkout() {
+        System.out.println("Checkout Method from ShoppingCart Called ...");
+    }
+}
