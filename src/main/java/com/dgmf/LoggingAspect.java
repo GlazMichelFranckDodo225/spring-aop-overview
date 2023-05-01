151088feadfcb10501ea8b68440524d747d036d3
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-01 06:30:19 +0200

    Created the cross-cutting concern (aspect) LoggingAspect (having the @Aspect annotation) and responsible for the connection

---
+++ src/main/java/com/dgmf/LoggingAspect.java
@@ -0,0 +1,7 @@
+package com.dgmf;
+
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+}
--- src/main/java/com/dgmf/ShoppingCart.java
+++ src/main/java/com/dgmf/ShoppingCart.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ShoppingCart {
+    // Logging
+    // Authentication & Authorization
+    // Sanitize the Data
     public void checkout() {
         System.out.println("Checkout Method from ShoppingCart Called ...");
     }
