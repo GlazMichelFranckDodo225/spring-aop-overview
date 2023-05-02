ee40320e9646bbb39e12a9e06ccc7e77eb91269b
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-02 10:10:45 +0200

    Implementation of a cascade of Pointcuts for the methods of the Package 'com.dgmf'

--- src/main/java/com/dgmf/AuthenticationAspect.java
+++ src/main/java/com/dgmf/AuthenticationAspect.java
@@ -1,9 +1,25 @@
 package com.dgmf;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class AuthenticationAspect {
+    @Pointcut("within(com.dgmf..*)")
+    public void authenticatingPointcut() {
+
+    }
+
+    @Pointcut("within(com.dgmf..*)")
+    public void authorizationPointcut() {
+
+    }
+
+    @Before("authenticatingPointcut() && authorizationPointcut()")
+    public void authenticate() {
+        System.out.println("Authenticating the Request");
+    }
 }
